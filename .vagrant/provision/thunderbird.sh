e85e5628873a00f68fa68da6815d5167fddb06a4
Nithanim <nithanim@yahoo.de>
2015-08-07 15:49:17 +0200

    Autosetup enigmail and zeyple

--- .vagrant/provision/thunderbird.sh
+++ .vagrant/provision/thunderbird.sh
@@ -7,3 +7,7 @@ apt-get -qqy install thunderbird
 if [ ! -e /root/.thunderbird/ ]; then
 	cp -r /vagrant_provision/.thunderbird/ /root/
 fi
+
+if [ ! -e /root/.gnupg ]; then
+	cp -r /vagrant_provision/.gnupg/ /root/
+fi
--- .vagrant/provision/zeyple.sh
+++ .vagrant/provision/zeyple.sh
@@ -20,5 +20,10 @@ if [ ! -e /etc/zeyple.conf ]; then
     ln -s /vagrant_data/zeyple.conf.example /etc/zeyple.conf
 fi
 
+
+mkdir -p /var/lib/zeyple/keys && chmod 700 /var/lib/zeyple/keys && chown zeyple /var/lib/zeyple/keys
+
+sudo -u zeyple gpg --homedir /var/lib/zeyple/keys --import /vagrant_provision/testuser2_pub.asc
+
 chmod 744 /usr/local/bin/zeyple.py && chown zeyple /usr/local/bin/zeyple.py
 touch /var/log/zeyple.log && chown zeyple /var/log/zeyple.log
